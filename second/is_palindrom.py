c65a20619b9b62a0ebc9f8ef2900b2fa16514265
Andrew Maslianko <maslianko.andrew@gmail.com>
2024-07-28 17:21:27 +0300

    Fix comment for second task

--- second/is_palindrom.py
+++ second/is_palindrom.py
@@ -3,6 +3,8 @@ from collections import deque
 def is_palindrom(s):
     d =  deque()
     for c in s:
+        if c == ' ':
+            continue
         d.append(c.lower())
     
     while len(d) > 1:
